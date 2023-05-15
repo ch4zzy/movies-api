f5a48b8cab053ce8b86b2c79c1e3eb7192b410a0
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-05-15 19:02:39 +0300

    - feat(utils.py): add functions to load actors, genres, and movies from JSON files and save them to the database.

---
+++ apps/movie/utils.py
@@ -0,0 +1,78 @@
+import json
+from datetime import datetime
+
+from apps.movie.models import Actor, Genre, Movie, MovieActor, MovieGenre
+
+
+def load_actors_from_json(file_path):
+    """
+    Load actor data from a JSON file and save it to the database.
+    """
+    with open(file_path) as file:
+        data = json.load(file)
+        for item in data:
+            actor = Actor(name=item)
+            actor.save()
+
+
+def load_genre_from_json(file_path):
+    """
+    Load genre data from a JSON file and save it to the database.
+    """
+    with open(file_path) as file:
+        data = json.load(file)
+        for item in data:
+            genre = Genre(title=item)
+            genre.save()
+
+
+def load_movies_from_json(file_path):
+    """
+    Load movie data from a JSON file and save it to the database.
+    """
+    with open(file_path) as file:
+        data = json.load(file)
+        # item = data[0]
+        for item in data:
+            title = item["title"]
+            rating = item["rating"]
+            genres = item["genres"]
+            actors = item["actors"]
+            raw_date = item["release_date"]
+            try:
+                release_date = datetime.strptime(raw_date, "%d %b %Y").date()
+                movie = Movie(title=title, rating=rating, release_date=release_date)
+                movie.save()
+            except ValueError:
+                date = raw_date.strip()
+                release_date = f"{date}-01-01"
+                movie = Movie(title=title, rating=rating, release_date=release_date)
+                movie.save()
+            for genre_title in genres:
+                try:
+                    genre = Genre.objects.get(title=genre_title)
+                except Genre.DoesNotExist:
+                    genre = Genre(title=genre_title)
+                    genre.save()
+
+                MovieGenre.objects.create(movie=movie, genre=genre)
+
+            for actor_name in actors:
+                try:
+                    actor = Actor.objects.get(name=actor_name)
+                except Actor.DoesNotExist:
+                    actor = Actor(name=actor_name)
+                    actor.save()
+
+                MovieActor.objects.create(movie=movie, actor=actor)
+
+        """
+
+        for genre_title in genres:
+            genre, _ = Genre.objects.get_or_create(title=genre_title)
+            MovieGenre.objects.create(movie=movie, genre=genre)
+
+        for actor_name in actors:
+            actor, _ = Actor.objects.get_or_create(name=actor_name)
+            MovieActor.objects.create(movie=movie, actor=actor)
+        """
