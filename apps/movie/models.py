4b095da2ecc0794b920c483945b9337bc183d152
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-05-15 19:02:02 +0300

    - feat(models.py): add Actor, Genre, Movie, MovieActor, MovieGenre, and SimilarMovie models to the app/movie app.

--- apps/movie/models.py
+++ apps/movie/models.py
@@ -1,3 +1,48 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Actor(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    release_date = models.DateField()
+    rating = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(10.0)])
+    genre = models.ManyToManyField(Genre, through="MovieGenre")
+    actor = models.ManyToManyField(Actor, through="MovieActor")
+
+    def __str__(self):
+        return self.title
+
+
+class MovieActor(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    actor = models.ForeignKey(Actor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.movie.title
+
+
+class MovieGenre(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.movie.title
+
+
+class SimilarMovie(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    similar_movie = models.ForeignKey(Movie, related_name="similar_movies", on_delete=models.CASCADE)
