7f0b8b9baefc55534fa9b1a0354bf361e3d5b749
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-05-17 17:27:33 +0300

    - feat(models.py): add similar_movie field to Movie model - feat(models.py): add SimilarMovie model to represent the relationship between similar movies

--- apps/movie/models.py
+++ apps/movie/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 
 class Actor(models.Model):
+    """
+    Model representing an actor
+    """
+
     name = models.CharField(max_length=255)
 
     def __str__(self):
@@ -10,6 +14,10 @@ class Actor(models.Model):
 
 
 class Genre(models.Model):
+    """
+    Model representing a genre
+    """
+
     title = models.CharField(max_length=255)
 
     def __str__(self):
@@ -17,17 +25,26 @@ class Genre(models.Model):
 
 
 class Movie(models.Model):
+    """
+    Model representing a movie
+    """
+
     title = models.CharField(max_length=255)
     release_date = models.DateField()
     rating = models.FloatField(validators=[MinValueValidator(0.0), MaxValueValidator(10.0)])
     genre = models.ManyToManyField(Genre, through="MovieGenre")
     actor = models.ManyToManyField(Actor, through="MovieActor")
+    similar_movie = models.ManyToManyField("self", through="SimilarMovie", symmetrical=False)
 
     def __str__(self):
         return self.title
 
 
 class MovieActor(models.Model):
+    """
+    Model representing the relationship between a movie and actor
+    """
+
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     actor = models.ForeignKey(Actor, on_delete=models.CASCADE)
 
@@ -36,6 +53,10 @@ class MovieActor(models.Model):
 
 
 class MovieGenre(models.Model):
+    """
+    Model representing the relationship between a movie and a genre
+    """
+
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
 
@@ -44,5 +65,12 @@ class MovieGenre(models.Model):
 
 
 class SimilarMovie(models.Model):
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
-    similar_movie = models.ForeignKey(Movie, related_name="similar_movies", on_delete=models.CASCADE)
+    """
+    Model representing the relationship between similar movies
+    """
+
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="similar_movies")
+    similar_movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.movie.title} - {self.similar_movie.title}"
