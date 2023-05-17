dd20863f0d2e28a959955d02546356e920efb5a0
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-05-17 17:19:42 +0300

    edited: small changes in urls

--- apps/movie/urls.py
+++ apps/movie/urls.py
@@ -1,13 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
 
+from apps.movie.views import MovieViewSet
+
 app_name = "api_movie"
 
 router = routers.DefaultRouter()
-router.register(
-    r"movies",
-)
-
+router.register(r"movies", MovieViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
