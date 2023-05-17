1bdb2e09e92917606ef47218925525f3f92ebf25
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-05-17 17:24:45 +0300

    - feat(views.py): add MovieViewSet with CRUD operations and serializers for list and detail views

--- apps/movie/views.py
+++ apps/movie/views.py
@@ -1,1 +1,17 @@
-# Create your views here.
+from rest_framework import viewsets
+
+from apps.movie.models import Movie
+from apps.movie.serializer import MovieDetailSerializer, MovieListSerializer
+
+
+class MovieViewSet(viewsets.ModelViewSet):
+    """
+    Viewset for CRUD operations on Movie model using serializers for list and detail
+    """
+
+    queryset = Movie.objects.all()
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return MovieListSerializer
+        return MovieDetailSerializer
